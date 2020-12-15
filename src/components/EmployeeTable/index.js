eafbbbca8e8efd03f8c30570db1c32b81bf265cf
Tom Haigh <tmhaigh@gmail.com>
2020-12-15 16:38:39 +1100

    renamed to EmployeeTable

---
+++ src/components/EmployeeTable/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import TableRow from "../TableRow";
+import API from "../../utils/API";
+
+class EmployeeTable extends Component {
+
+    state = {
+        results: []
+    }
+
+    componentDidMount() {
+        this.getUsers();
+    }
+
+    getUsers = async () => {
+        try {
+            let users = await API.search();
+            let { results } = users.data
+            console.log(results)
+            this.setState({ results: results })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>DOB</th>
+                        <th>Age</th>
+                        <th>Gender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.results.map(({ id, name, email, dob, picture, gender }) => (
+                        <TableRow
+                            key={id.value}
+                            name={name}
+                            email={email}
+                            dob={dob}
+                            picture={picture}
+                            gender={gender} />
+                    ))}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default EmployeeTable;
