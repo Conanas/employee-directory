c6eae920d014d39303e57ac88834b743dfb6bca8
Tom Haigh <tmhaigh@gmail.com>
2020-12-18 09:21:39 +1100

    created sort form in SortOptions

--- src/components/SortOption/index.js
+++ src/components/SortOption/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./style.css";
 
-function SortOption() {
+function SortOption(props) {
     return (
-        <p>SortOption</p>
+        <form onSubmit={(event) => props.sortTable(event)}>
+            <label>Sort employees by: </label><br />
+            <input type="radio" name="ageName" defaultChecked value="name" id="name" />
+            <label>Name</label>
+            <input type="radio" name="ageName" value="age" id="age" />
+            <label>Age</label>
+            <input type="radio" name="ascDes" defaultChecked value="asc" id="asc" />
+            <label>Asc.</label>
+            <input type="radio" name="ascDes" value="desc" id="desc" />
+            <label>Desc.</label>
+            <input type="submit" value="Sort" />
+        </form>
     )
 }
 
