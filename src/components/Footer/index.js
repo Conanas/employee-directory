d78071270b8d7c3c16b911320f6ff0498e255ca0
Tom Haigh <tmhaigh@gmail.com>
2020-12-17 16:46:35 +1100

    turned into function

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style.css"
 
-class Footer extends Component {
-    render() {
-        return (
-            <header>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <h1>Footer</h1>
-                        </div>
+function Footer() {
+    return (
+        <header>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Footer</h1>
                     </div>
                 </div>
-            </header>
-        )
-    }
+            </div>
+        </header>
+    )
+
 }
 
 export default Footer
