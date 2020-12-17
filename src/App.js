0fd6a0fb3c8b27311bc19b178277f579d392197b
Tom Haigh <tmhaigh@gmail.com>
2020-12-18 10:31:37 +1100

    formatted code to indent by 2 spaces

--- src/App.js
+++ src/App.js
@@ -7,91 +7,91 @@ import API from "./utils/API"
 
 class App extends Component {
 
-	state = {
-		results: [],
-	}
+  state = {
+    results: [],
+  }
 
-	componentDidMount = () => {
-		this.getUsers();
-	}
+  componentDidMount = () => {
+    this.getUsers();
+  }
 
-	getUsers = async () => {
-		try {
-			let users = []
-			while (users.length === 0) {
-				users = await API.search();
-			}
-			let { results } = users.data
-			let tempResults = [];
-			results.forEach(({ dob, email, gender, id, name, picture }) => {
-				let date = new Date(dob.date)
-				let newUser = {
-					age: dob.age,
-					dob: `${date.getDate()}/${date.getMonth() + 1}/${date.getYear()}`,
-					email: email,
-					gender: gender,
-					id: id.value,
-					name: `${name.first} ${name.last}`,
-					picture: picture.medium
-				}
-				tempResults.push(newUser)
-			})
-			this.setState(prevState => prevState.results = tempResults)
-		} catch (error) {
-			console.log(error)
-		}
-	}
+  getUsers = async () => {
+    try {
+      let users = []
+      while (users.length === 0) {
+        users = await API.search();
+      }
+      let { results } = users.data
+      let tempResults = [];
+      results.forEach(({ dob, email, gender, id, name, picture }) => {
+        let date = new Date(dob.date)
+        let newUser = {
+          age: dob.age,
+          dob: `${date.getDate()}/${date.getMonth() + 1}/${date.getYear()}`,
+          email: email,
+          gender: gender,
+          id: id.value,
+          name: `${name.first} ${name.last}`,
+          picture: picture.medium
+        }
+        tempResults.push(newUser)
+      })
+      this.setState(prevState => prevState.results = tempResults)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-	sortTable = (event) => {
-		event.preventDefault()
-		let { target } = event
-		let { name, asc } = target
-		let tempResults = this.state.results
-		let lessThan = 1
-		let greaterThan = -1
-		if (asc.checked) {
-			lessThan = -1
-			greaterThan = 1
-		}
-		tempResults.sort((a, b) => {
-			let itemA = a.age
-			let itemB = b.age
-			if (name.checked) {
-				itemA = a.name.toLowerCase()
-				itemB = b.name.toLowerCase()
-			}
-			return (
-				itemA < itemB ? lessThan :
-					itemA > itemB ? greaterThan :
-						0
-			)
+  sortTable = (event) => {
+    event.preventDefault()
+    let { target } = event
+    let { name, asc } = target
+    let tempResults = this.state.results
+    let lessThan = 1
+    let greaterThan = -1
+    if (asc.checked) {
+      lessThan = -1
+      greaterThan = 1
+    }
+    tempResults.sort((a, b) => {
+      let itemA = a.age
+      let itemB = b.age
+      if (name.checked) {
+        itemA = a.name.toLowerCase()
+        itemB = b.name.toLowerCase()
+      }
+      return (
+        itemA < itemB ? lessThan :
+          itemA > itemB ? greaterThan :
+            0
+      )
 
-		})
-		this.setState(prevState => prevState.results = tempResults)
-	}
+    })
+    this.setState(prevState => prevState.results = tempResults)
+  }
 
-	render() {
-		return (
-			<>
-				<Header />
-				<main>
-					<div className="container">
-						<div className="row">
-							<div className="col-md-12">
-								<SortOption
-									sortTable={this.sortTable}
-								/>
-								<EmployeeTable
-									results={this.state.results}
-								/>
-							</div>
-						</div>
-					</div>
-				</main>
-				<Footer />
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <Header />
+        <main>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <SortOption
+                  sortTable={this.sortTable}
+                />
+                <EmployeeTable
+                  results={this.state.results}
+                />
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </>
+    );
+  }
 
 }
 
--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -3,33 +3,33 @@ import TableRow from "../TableRow";
 import "./style.css";
 
 function EmployeeTable({ results, sortTable }) {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Picture</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>DOB</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                </tr>
-            </thead>
-            <tbody>
-                {results.map(({ age, dob, email, gender, id, name, picture }) => (
-                    <TableRow
-                        age={age}
-                        dob={dob}
-                        email={email}
-                        gender={gender}
-                        key={id}
-                        name={name}
-                        picture={picture}
-                    />
-                ))}
-            </tbody>
-        </table>
-    )
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>DOB</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map(({ age, dob, email, gender, id, name, picture }) => (
+          <TableRow
+            age={age}
+            dob={dob}
+            email={email}
+            gender={gender}
+            key={id}
+            name={name}
+            picture={picture}
+          />
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 export default EmployeeTable;
