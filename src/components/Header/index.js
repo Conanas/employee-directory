8851c611fa1de3a2c061ecc9dd699f47ffdc5234
Tom Haigh <tmhaigh@gmail.com>
2020-12-21 15:53:52 +1100

    added second col to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,11 @@ function Header() {
         <header>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12">
+                    <div className="col-md-6">
                         <h1>Employee Directory</h1>
+                    </div>
+                    <div className="col-md-6 d-flex justify-content-sm-end align-items-sm-center">
+                        <label>A product by Tom Haigh</label>
                     </div>
                 </div>
             </div>
