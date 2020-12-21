0313121cc5015a8bbb2efaa72afbe9d9680ed4e9
Tom Haigh <tmhaigh@gmail.com>
2020-12-21 16:02:19 +1100

    added flex row reverse

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,14 +5,7 @@ function Footer() {
   return (
     <footer>
       <div className="container">
-        <div className="row">
-          {/* </div>
-        <div className="row"> */}
-          <div className="col-sm-6 d-flex flex-column">
-            <label>Phone: 0406 253 030</label>
-            <label>Email: tmhaigh@gmail.com</label>
-            <label>Location: Melbourne, Victoria, Australia</label>
-          </div>
+        <div className="row d-flex flex-sm-row-reverse">
           <div className="col-sm-6 d-flex justify-content-sm-end align-items-sm-center">
             <a className="links-div-anchor" href="https://www.linkedin.com/in/thomas-haigh-5960b81aa/" target="_blank" rel="noreferrer">
               <i className="fab fa-linkedin font-awesome social-icon"></i>
@@ -20,6 +13,11 @@ function Footer() {
             <a className="links-div-anchor" href="https://github.com/Conanas" target="_blank" rel="noreferrer">
               <i className="fab fa-github font-awesome social-icon"></i>
             </a>
+          </div>
+          <div className="col-sm-6 d-flex flex-column">
+            <label>Phone: 0406 253 030</label>
+            <label>Email: tmhaigh@gmail.com</label>
+            <label>Location: Melbourne, Victoria, Australia</label>
           </div>
         </div>
       </div>
