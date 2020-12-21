df8d3e95301efa82cd8f5016d28d50d6d04b6683
Tom Haigh <tmhaigh@gmail.com>
2020-12-21 15:52:23 +1100

    added bootstrap div rows and cols

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,21 +3,27 @@ import "./style.css"
 
 function Footer() {
   return (
-    <footer>
-      <div>
-        <a className="links-div-anchor" href="https://www.linkedin.com/in/thomas-haigh-5960b81aa/" target="_blank" rel="noreferrer">
-          <i className="fab fa-linkedin font-awesome social-icon"></i>
-        </a>
-        <a className="links-div-anchor" href="https://github.com/Conanas" target="_blank" rel="noreferrer">
-          <i className="fab fa-github font-awesome social-icon"></i>
-        </a>
+    <footer className="header-footer">
+      <div className="container">
+        <div className="row">
+          {/* </div>
+        <div className="row"> */}
+          <div className="col-sm-6 d-flex flex-column">
+            <label>Phone: 0406 253 030</label>
+            <label>Email: tmhaigh@gmail.com</label>
+            <label>Location: Melbourne, Victoria, Australia</label>
+          </div>
+          <div className="col-sm-6 d-flex justify-content-sm-end align-items-sm-center">
+            <a className="links-div-anchor" href="https://www.linkedin.com/in/thomas-haigh-5960b81aa/" target="_blank" rel="noreferrer">
+              <i className="fab fa-linkedin font-awesome social-icon"></i>
+            </a>
+            <a className="links-div-anchor" href="https://github.com/Conanas" target="_blank" rel="noreferrer">
+              <i className="fab fa-github font-awesome social-icon"></i>
+            </a>
+          </div>
+        </div>
       </div>
-      <div>
-        <label>Phone: 0406 253 030 || </label>
-        <label>Email: tmhaigh@gmail.com || </label>
-        <label>Location: Melbourne, Victoria, Australia</label>
-      </div>
-    </footer>
+    </footer >
   )
 
 }
